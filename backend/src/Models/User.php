f972b32c8a0b7fd1fc91f17014006c22ba50a7fc
Artem <vip.artem4567@mail.ru>
2025-08-03 13:47:07 +0600

    METHODS FOR AUTHORZATION

---
+++ backend/src/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+    // КЛАСС ЮЗЕРА
+    namespace App\Models;
+
+    use App\Services\DataBase;
+    use PDO;
+
+    class User {
+        // функция поиска email
+        public static function findByEmail(string $email):array{
+            $pdo = DataBase::Connection();
+            $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
+            $stmt->execute([$email]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        // фунекция создания пользователя
+        public static function create( string $name, string $email, string $passwordHash){
+            $pdo = DataBase::Connection();
+            $stmt = $pdo->prepare('INSERT INTO users (name, email, password) VALUES(:name,:email,:password)');
+            $stmt->execute([$name,$email,$passwordHash]);
+        }
+    }
+?>
---
+++ backend/src/Services/SessionService.php
@@ -0,0 +1,39 @@
+<?php
+    // КЛАСС УПАРВЛЕНИЕ СЕССИЯМИ 
+    namespace App\Services;
+    use App\Services\DataBase;
+
+    class SessionService{
+        // генерация токена
+        public static function generateToken(){
+            return bin2hex(random_bytes(32));
+        }
+        // функция создания сессии в базу с жизнеспособностью 30 дней 
+        public static function create(string $token, int $user_id){
+            $pdo = DataBase::Connection();
+            $stmt = $pdo->prepare('INSER INTO session (token,user_id,expires_at) VALUES (?,?,?)');
+            $stmt->execute([
+                $token,
+                $user_id,
+                date("Y-m-d H:i:s",strtotime('+30 days'))
+                // форматация даты и прибавка 30 дней 
+            ]);
+        }
+        // ФУНКЦИЯ ДЯЛ ПОРЛУЧЕНИЯ ID БЗЕРА ИЗ СЕССИИ
+        public static function getUserById(string $token){
+            $pdo = DataBase::Connection();
+            // ВЫБЕРИ МНЕ ID ЮЗЕРА И ПОДСЬАВЬ ЕГО ТОКЕН И ЕСЛИ ЕГО СЕСССИЯ ЕЩЕ НЕ ИСТЕКЛА 
+            $stmt = $pdo->prepare("SELECT user_id FROM session WHERE token = ? AND expires_at > NOW()");
+            $stmt->execute([$token]); // подставляет токен в ?
+            // ПОЛУЧАЕМ ПЕРВУЮ ЗАПИСЬ В ТАБЛИЦЕ 
+            $row = $stmt->fetch();
+            return $row ? (int)$row['user_id'] : null;
+        }
+        // ФУНКЦИЯ УДЛАЕНИЯ СЕСИИИ И ЮЗЕРА ИЗ БД
+        public static function revoke(string $token) {
+            $pdo = DataBase::Connection();
+            $stmt = $pdo->prepare('DELETE FROM session WHERE token = ?');
+            $stmt->execute([$token]);
+        }
+    }
+?>
