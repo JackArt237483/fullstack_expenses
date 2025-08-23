cc26d43360d4d022eb32424b9dad0aac139d1bbd
Artem <vip.artem4567@mail.ru>
2025-08-23 23:58:12 +0600

    function for debug api

--- frontend/skills/src/utills/fetchWithAuth.js
+++ frontend/skills/src/utills/fetchWithAuth.js
@@ -1,5 +1,20 @@
 import { useAuthStore } from "@/stores/auth.js";
-// help functions with api
+// help functions with apiз
 export const fetchWithAuth = async (url,options={}) =>{
+  const auth = useAuthStore()
 
+  // включить все заголовки для запроса
+  const headers = {
+    ...(options.headers || {}),
+    Authorization: auth.token,
+    'Content-Type': 'application/json'
+  }
+  const res = await  fetch(url,{
+    ...options,
+    headers
+  })
+
+  if(!res.ok) throw new Error(`Ошибка запроса: ${res.status}`)
+  // Возвращаешь тело ответа в виде JSON
+  return await res.json()
 }
