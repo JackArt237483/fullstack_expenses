0115310c0d7ba83994e97f3e77390abe29ffef8d
Artem <vip.artem4567@mail.ru>
2025-08-28 23:40:48 +0600

    update service and controller with expense

--- backend/src/Controllers/ExpenseController.php
+++ backend/src/Controllers/ExpenseController.php
@@ -1,58 +1,76 @@
 <?php
 namespace App\Controllers;
 
-use App\Models\Expense;
-
+use App\Services\ExpenseServices;
 class ExpenseController{
-    // get запрос
+    public function __construct(private ExpenseServices $services){}
+    // Обрабатывает GET-запрос для получения всех расходов
     public function index(){
-        $expense = Expense::all();
+        $expense = $this->services->getAll();
         // получаем все записи
-        echo json_encode($expense);
+        echo $this->json($expense);
         // отправляем все записи в json
     }
-    // post запрос
-    public function store()
-    {
-        // штука которая полчаает из инпута данные
-        $data = json_decode(file_get_contents("php://input"),true);
-
-        if(!isset($data['title'],$data['amount'],$data['category_id'])
-            || trim($data['title'] === "" ||
-            (float) $data['amount'] <= 0)
-        ){
-            http_response_code(400);
-            echo json_encode(['error' => 'error man']);
+    // Обрабатывает GET-запрос получения расходов конкретного пользовател
+    public function getById(int $user_id){
+        $expense = $this->services->getExpenseById($user_id);
+        if(empty($expense)){
+            $this->json(['error' => 'У юзера нет расходов'], 404);
             return;
         }
 
-        // метод создания записи
-        $success = Expense::create($data['title'],(float)$data['amount'],(int)$data['category_id']);
-        // ПРЕОБРАЗОВАНИЯ В JSON
-        echo json_encode(['status' => $success ? 'success' : 'error']);
+        $this->json($expense);
+    }
+    // Обрабатывает POST-запрос для создания нового расхода.
+    public function store()
+    {
+        $data = $this->jsonInput();
+
+        $result = $this->services->createExpense($data);
+
+        if(!$result){
+            $this->json(['error' => 'Неврзможно моздать расход'],404);
+            return;
+        }
+
+        $this->json(['status' => 'created'],200);
     }
     // delete запрос
     public function delete($id)
     {
-        if(Expense::delete($id)){
-            echo json_encode(['status' => 'success']);
-        } else{
-            http_response_code(404);
-            echo json_encode(['error' => 'error man']);
+        $result = $this->services->deleteExpense($id);
+
+        if(!$result){
+            $this->json(['error' => 'Не удалить расход по id']);
+            return;
         }
+
+        $this->json(['status' => 'deleted'],);
     }
-    // put запрос
+    // Обрабатывает PUT-запрос для обновления существующего расхода.
     public function update($id)
     {
         // метод получение данных из инпутов
-        $data = json_decode(file_get_contents("php://input"),true);
+        $data = $data = $this->jsonInput();
         // валидация запроса проверка JSON
-        if(!isset($data['title'],$data['amount'])){
-            http_response_code(404);
-            echo json_encode(['error' => 'just mistake man']);
+        $result = $this->services->updateExpense($id,$data);
+        if(!$result){
+            $this->json(['error' => 'Неврзможно оновить расход'],404);
             return;
         }
-        $success = Expense::update($id,$data['title'],(float)$data['amount']);
-        echo json_encode(['status' => $success ? 'success' : 'error']);
+
+        $this->json(['status' => 'created']);
+    }
+    private function jsonInput()
+    {
+        // штука которая полчаает из инпута данные
+        $input = file_get_contents("php://input");
+        return json_decode($input,true) ?? [];
+    }
+    // Универсальный метод для отправки JSON-ответов клиенту.
+    private function json(array $data, int $code = 200){
+        http_response_code($code);
+        header("Content-Type: application/json");
+        echo json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 }
--- backend/src/Services/ExpenseServices.php
+++ backend/src/Services/ExpenseServices.php
@@ -5,8 +5,53 @@
 
  class ExpenseServices{
     public function __construct(private ExpenseRepository $repo){}
+    // Метод возрощает записи все из репозитория
      public function getAllExpense():array
      {
          return $this->repo->getAll();
      }
+     //  Метод получение записей по id юзера
+     public function getExpenseById(int $user_id):array
+     {
+         // проверка то что id больше 0
+        if($user_id <= 0) return [];
+        // если id валиден то вызывается метод
+        return $this->repo->getUserById($user_id);
+     }
+     //  Метод Создает новый расход
+     public function createExpense(array $data):bool{
+          if(
+              empty(
+            (string)$data['title'] ||
+            (float)$data['amount'] <= 0 ||
+            (int)$data['category_id'] <= 0 ||
+            (int)$data['user_id'] <= 0)
+          )
+        {
+            return false;
+        }
+          return $this->repo->create(
+              (string)$data['title'],
+              (float)$data['amount'],
+              (int)$data['category_id'],
+              (int)$data['user_id']
+          );
+     }
+     // Метод обновлет существующий расход
+     public function updateExpense(int $id, array $data):bool
+     {
+        if(
+            $id <= 0 ||
+            empty($data['title']) ||
+            (float)$data['amount'] <= 0)
+        {
+            return false;
+        }
+        return $this->repo->update($id, $data['title'], (float)$data['amount']);
+     }
+     // Метод удаляет существующий расход по id.
+    public function deleteExpense(int $id):bool {
+        if($id <= 0) return false;
+        return $this->repo->delete($id);
+    }
  }
